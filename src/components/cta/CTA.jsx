f1dc718bd9064fa6b67e4b9f079a268c2c5bc480
Barissa-Imran <imranbarissa@gmail.com>
2023-01-17 13:33:29 +0300

    Add react containers to completion Add imports fo blog

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -4,7 +4,15 @@ import './cta.css';
 
 const CTA = () => {
     return (
-        <div>CTA</div>
+        <div className='gpt3__cta'>
+            <div className="gpt3__cta-content">
+                <p>Request Early Access</p>
+                <h3>Register today & start exploring the endless possiblities.</h3>
+            </div>
+            <div className='gpt3__cta-button'>
+                <button type='buttn'>Get Started</button>
+            </div>
+        </div>
     )
 }
 
