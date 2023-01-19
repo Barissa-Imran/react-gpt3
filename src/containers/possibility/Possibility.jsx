f18af88097438e376a0964e228150d0b1d7b9f46
Rahulbaran <rahulkumar109422@gmail.com>
2023-01-19 18:35:08 +0530

    perf: import webp format of image
    - imported possibility image in webp
    - added loading and decoding attributes in image tag

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 import './possibility.css';
-import possibilityImage from '../../assets/possibility.png'
+import possibilityImage from '../../assets/possibility.webp'
 
 const Possibility = () => {
     return (
         <div className='gpt3__possibility section__padding' id='possibility'>
             <div className="gpt3__possibility-image">
-                <img src={possibilityImage} alt="possibility" />
+                <img src={possibilityImage} alt="possibility" loading="lazy" decoding="async" />
             </div>
             <div className="gpt3__possibility-content">
                 <h4>Request Early Access to Get Started</h4>
